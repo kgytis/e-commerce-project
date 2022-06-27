6d52bb1a4b31f42b92a8f0c80014f361702c0ed4
kgytis <kaziukon.gytis@gmail.com>
2022-06-27 17:27:06 +0300

    GET item by ID

--- server/src/controllers/API/items.ts
+++ server/src/controllers/API/items.ts
@@ -1,14 +1,14 @@
 import mysql from "mysql2/promise";
 import mysqlConfig from "../../dbConfig.js";
 
-const allItems = async (req: any, res: any) => {
+const allItems = async (req: any, res: any): Promise<void> => {
   try {
     const con = await mysql.createConnection(mysqlConfig);
-    const sql = `
+    const sql: string = `
         SELECT *
         FROM item
         `;
-    const [data] = await con.query(sql);
+    const [data]: object[] = await con.query(sql);
     await con.end();
     res.send(data);
   } catch (err) {
@@ -16,4 +16,21 @@ const allItems = async (req: any, res: any) => {
   }
 };
 
-export { allItems };
+const getItemByID = async (req: any, res: any): Promise<void> => {
+  try {
+    const itemId: string = req.params.id;
+    const con = await mysql.createConnection(mysqlConfig);
+    const sql: string = `
+          SELECT *
+          FROM item
+          WHERE id = ?
+          `;
+    const [data]: object[] = await con.query(sql, itemId);
+    await con.end();
+    res.send(data);
+  } catch (err) {
+    res.send({ err: err });
+  }
+};
+
+export { allItems, getItemByID };
--- server/src/routes/API/items.ts
+++ server/src/routes/API/items.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
 //Controller imports
-import { allItems } from "../../controllers/API/items.js";
+import { allItems, getItemByID } from "../../controllers/API/items.js";
 
 const apiItemsRouter = express.Router();
 
 apiItemsRouter.get("/items", allItems);
+apiItemsRouter.get("/items/:id", getItemByID);
 
 export default apiItemsRouter;
